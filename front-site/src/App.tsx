6a71b07c394272ef66a89c9e361cf7e96d0dd718
Leonardo <leoyasu@yahoo.com.br>
2020-11-07 12:43:15 +0000

    React site

---
+++ front-site/src/App.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          MEGA HACK 5.0.
+        </p>
+        <a
+          className="App-link"
+          href="/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Projeto TOOLS
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;

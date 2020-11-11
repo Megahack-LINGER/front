80758d0884f201497f9529596356430935684786
Ramon Paolo Maran <ramonpaolomaran12@gmail.com>
2020-11-11 14:34:25 -0300

    Add model product

---
+++ src/pages/models/product/Product.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+import "./Product.css"
+
+import image1 from "../../../assets/images/maquina-laser.jpg";
+
+
+export default class Product extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            title: props.title,
+            author: props.author,
+            price: props.price,
+            subtitle: props.subtitle
+        }
+    }
+
+    render() {
+        return (<div id="product" >
+
+            <figure>
+                <img id="image-product" src={image1} alt={this.state.title} />
+            </figure>
+
+            <hr color="#1768ac" />
+
+            <h4 id="title" >{this.state.title}</h4>
+            <p id="subtitle" >{this.state.subtitle}</p>
+            <p id="price" >R${this.state.price}</p>
+
+        </div>)
+    }
+}

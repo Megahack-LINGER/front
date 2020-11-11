862a578d6b3d7be0b9341e05efa0a5484e234c94
Ramon Paolo Maran <ramonpaolomaran12@gmail.com>
2020-11-11 14:36:55 -0300

    Add content center

--- src/pages/models/product/Product.jsx
+++ src/pages/models/product/Product.jsx
@@ -19,17 +19,17 @@ export default class Product extends React.Component {
 
     render() {
         return (<div id="product" >
+            <center>
+                <figure>
+                    <img id="image-product" src={image1} alt={this.state.title} />
+                </figure>
 
-            <figure>
-                <img id="image-product" src={image1} alt={this.state.title} />
-            </figure>
+                <hr color="#1768ac" />
 
-            <hr color="#1768ac" />
-
-            <h4 id="title" >{this.state.title}</h4>
-            <p id="subtitle" >{this.state.subtitle}</p>
-            <p id="price" >R${this.state.price}</p>
-
+                <h4 id="title" >{this.state.title}</h4>
+                <p id="subtitle" >{this.state.subtitle}</p>
+                <p id="price" >R${this.state.price}</p>
+            </center>
         </div>)
     }
 }

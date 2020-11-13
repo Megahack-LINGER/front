0cacb538ff13e17b4afae265221207bce611880b
Ramon Paolo Maran <ramonpaolomaran12@gmail.com>
2020-11-13 09:18:11 -0300

    Add firestore

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -8,7 +8,6 @@ import "./Home.css";
 
 import Product from "../models/product/Product";
 
-<<<<<<< HEAD
 import firebase from "../../services/FirebaseServices";
 
 interface State {
@@ -21,42 +20,6 @@ interface State {
     image: Array<string>;
     price: number;
   }[];
-=======
-function Home() {
-  return (
-    <div>
-        <img
-          src="https://raw.githubusercontent.com/Megahack-LINGER/docs/main/imagens/BANNER_CORTADORA_A_LASER.jpg"
-          alt="logo-linger"
-          width="100%"
-        />
-  
-      <figure>
-        <img
-          src="https://raw.githubusercontent.com/Megahack-LINGER/docs/main/imagens/logo-linger.png"
-          alt="logo-linger"
-          width="200"
-        />
-      </figure>
-
-      <Product
-        title="Máquina cortadora a laser"
-        author="Leonardo joão IV"
-        subtitle="Máquina de fazer picadinho de madeira a laiser"
-        price="24.99"
-      />
-
-      <Product
-        title="Perfume blacon"
-        author="Ramon dom joão 1"
-        subtitle="emprestasse perfume da marca Blacon"
-        price="299.98"
-      />
-      
-      <Link to="/busca">Busca avançada</Link>
-    </div>
-  );
->>>>>>> 0cdac5891b44f15dda94f6b47036d767c7a13367
 }
 
 export default class Home extends React.Component<State> {

a128b8df80ac24c505d9a558964bde2e0b71799f
Leonardo <leonardo@lymtec.com.br>
2020-11-13 04:06:34 +0000

    Footer

--- src/pages/models/footer/Footer.jsx
+++ src/pages/models/footer/Footer.jsx
@@ -4,7 +4,7 @@ import "./Footer.css"
 
 export default function Footer() {
   return <footer>
-    <h1>Mega Hack 5.0 - Projeto Sebrae</h1>
+    <h1>Mega Hack 5.0 - Desafio Inovativa</h1>
     <h3>Portal de dúvidas</h3>
     <p>Email: <i>linger@gmail.com</i> | Whatsapp: <i>11 99898-9197</i></p>
   </footer>

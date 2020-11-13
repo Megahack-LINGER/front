e6fdd0373c0eb7d9c0ba2c579de9de2f9f15850e
Ramon Paolo Maran <ramonpaolomaran12@gmail.com>
2020-11-13 08:53:28 -0300

    Add firestore

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -6,43 +6,70 @@ import { Link } from "react-router-dom";
 //---- Styles
 import "./Home.css";
 
-//--- Images
-import image1 from "../../assets/images/maquina-laser.jpg";
-import image2 from "../../assets/images/maquina-sublimacao.jpg";
-import image3 from "../../assets/images/impressora3d.jpg";
-
-import person1 from "../../assets/images/sublimacao.jpg";
-import logo1 from "../../assets/images/logo-linger-2.png";
 import Product from "../models/product/Product";
 
-function Home() {
-  return (
-    <div>
-      <figure>
-        <img
-          src="https://raw.githubusercontent.com/Megahack-LINGER/docs/main/imagens/logo-linger.png"
-          alt="logo-linger"
-          width="200"
-        />
-      </figure>
+import firebase from "../../services/FirebaseServices";
 
-      <Product
-        title="Máquina cortadora a laser"
-        author="Leonardo joão IV"
-        subtitle="Máquina de fazer picadinho de madeira a laiser"
-        price="24.99"
-      />
-
-      <Product
-        title="Perfume blacon"
-        author="Ramon dom joão 1"
-        subtitle="emprestasse perfume da marca Blacon"
-        price="299.98"
-      />
-      
-      <Link to="/busca">Busca avançada</Link>
-    </div>
-  );
+interface State {
+  products: {
+    id: string;
+    title: string;
+    subtitle: string;
+    id_author: string;
+    cep_origem: string;
+    image: Array<string>;
+    price: number;
+  }[];
 }
 
-export default Home;
+export default class Home extends React.Component<State> {
+  state: State = {
+    products: [],
+  };
+
+  async getDatas() {
+    var datas = await firebase.firestore().collection("products").get();
+    datas.forEach((item) => {
+      this.setState({ products: [...this.state.products, item.data()] });
+      console.log(this.state.products);
+    });
+    this.state.products.map((item) => {
+      console.log(item.title);
+    });
+  }
+
+  async componentDidMount() {
+    await this.getDatas();
+  }
+
+  render() {
+    return (
+      <div>
+        <figure>
+          <img
+            src="https://raw.githubusercontent.com/Megahack-LINGER/docs/main/imagens/logo-linger.png"
+            alt="logo-linger"
+            width="200"
+          />
+        </figure>
+
+        <div id="row">
+          {this.state.products.map((item) => {
+            return (
+              <Product
+                title={item.title}
+                author="Leonardo joão IV"
+                subtitle={item.subtitle}
+                price={item.price}
+                image={item.image}
+                key={item.id}
+              />
+            );
+          })}
+        </div>
+
+        <Link to="/busca">Busca avançada</Link>
+      </div>
+    );
+  }
+}
--- src/pages/models/product/Product.jsx
+++ src/pages/models/product/Product.jsx
@@ -1,35 +1,37 @@
-import React from 'react';
+import React from "react";
 
-import "./Product.css"
+import "./Product.css";
 
 import image1 from "../../../assets/images/maquina-laser.jpg";
 
 
 export default class Product extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      title: props.title,
+      author: props.author,
+      price: props.price,
+      subtitle: props.subtitle,
+      image: props.image,
+    };
+  }
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            title: props.title,
-            author: props.author,
-            price: props.price,
-            subtitle: props.subtitle
-        }
-    }
+  render() {
+    return (
+      <div id="product">
+        <center>
+          <figure>
+            <img id="image-product" src={this.state.image[0]} alt={this.state.image[0]} />
+          </figure>
 
-    render() {
-        return (<div id="product" >
-            <center>
-                <figure>
-                    <img id="image-product" src={image1} alt={this.state.title} />
-                </figure>
+          <hr color="#1768ac" />
 
-                <hr color="#1768ac" />
-
-                <h4 id="title" >{this.state.title}</h4>
-                <p id="subtitle" >{this.state.subtitle}</p>
-                <p id="price" >R${this.state.price}</p>
-            </center>
-        </div>)
-    }
+          <h4 id="title">{this.state.title}</h4>
+          <p id="subtitle">{this.state.subtitle}</p>
+          <p id="price">R${this.state.price}</p>
+        </center>
+      </div>
+    );
+  }
 }

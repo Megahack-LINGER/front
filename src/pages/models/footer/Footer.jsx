520cfc27f3cf6f168adcd30a14b7317bedf33079
Ramon Paolo Maran <ramonpaolomaran12@gmail.com>
2020-11-14 15:21:29 -0300

    Add qrcode to link apk

--- src/pages/models/footer/Footer.jsx
+++ src/pages/models/footer/Footer.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import qrcode from "../../../assets/images/qrcode.svg"
+
 import "./Footer.css"
 
 export default function Footer() {
@@ -7,5 +9,7 @@ export default function Footer() {
     <h1>Mega Hack 5.0 - Desafio Inovativa</h1>
     <h3>Portal de dúvidas</h3>
     <p>Email: <i>linger@gmail.com</i> | Whatsapp: <i>11 99898-9197</i></p>
+    <span>Baixe o nosso App aqui</span>
+    <img src={qrcode} width="100vw" />
   </footer>
 }

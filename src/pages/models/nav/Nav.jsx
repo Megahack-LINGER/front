1b189666d3bbc877a155ddde66fb1d923d73ab9d
Ramon Paolo Maran <ramonpaolomaran12@gmail.com>
2020-11-14 12:46:54 -0300

    Add search product

--- src/pages/models/nav/Nav.jsx
+++ src/pages/models/nav/Nav.jsx
@@ -1,14 +1,24 @@
 //---- Packages
-import React from 'react';
-import { BrowserRouter as Router, Link, Switch } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, Redirect, Switch } from 'react-router-dom';
 
 //---- Styles
 import "./Nav.css"
 
 export default function Nav() {
+    const [searchProduct, setSearchProduct] = useState("")
     return <div id="nav">
         <Switch>
             <nav>
+                <input placeholder="Pesquise aqui" type="text" onChange={(e) =>
+                    setSearchProduct(e.target.value)} />
+                {searchProduct != "" ? <Redirect
+                    to={{
+                        pathname: "/busca",
+                        search: searchProduct,
+                        state: { text: searchProduct }
+                    }}
+                ></Redirect> : <Redirect to="/"></Redirect>}
                 <Link to="/">Principal</Link>
                 <Link to="/about">Sobre</Link>
                 <Link to="/contact"> Contato</Link>

0bcb93d561c085a7e9ef4057d5a54b8cc83fcb54
Ramon Paolo Maran <ramonpaolomaran12@gmail.com>
2020-11-14 20:39:33 -0300

    Att size map

--- src/pages/product/ScreenProduct.jsx
+++ src/pages/product/ScreenProduct.jsx
@@ -26,10 +26,10 @@ export default class Produto extends React.Component {
                 <figure id="image-produto">
                     <img id="image-produto" src={this.state.image[0]} />
                 </figure>
-    <h1>{this.state.title}</h1>
-    <h3>{this.state.subtitle}</h3>
-    <p>R${this.state.price} por hora</p>
-                <MapContainer center={[this.state.lat, this.state.lon]} zoom={12} style={{ width: "60vw", height: "60vh", borderRadius: "20px" }} >
+                <h1>{this.state.title}</h1>
+                <h3>{this.state.subtitle}</h3>
+                <p>R${this.state.price} por hora</p>
+                <MapContainer center={[this.state.lat, this.state.lon]} zoom={12} >
                     <TileLayer
                         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
